a469c945d96f2e702f1bd56d065300532cef0745
zan.dai <zan.dai@baibaomen.com>
2021-04-25 14:23:30 +0800

    添加项目文件。

---
+++ Billiard.Assistant/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Billiard.Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbConsole = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbConsole
+            // 
+            this.rtbConsole.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbConsole.Location = new System.Drawing.Point(12, 12);
+            this.rtbConsole.Name = "rtbConsole";
+            this.rtbConsole.ReadOnly = true;
+            this.rtbConsole.Size = new System.Drawing.Size(284, 168);
+            this.rtbConsole.TabIndex = 0;
+            this.rtbConsole.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 192);
+            this.Controls.Add(this.rtbConsole);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbConsole;
+    }
+}
+
---
+++ Billiard.Assistant/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Billiard.Assistant
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        static KeyboardHook keyboardHook;
+
+        int ra = 25;
+        int index = 0;
+        Point startMousePosition;
+        Point endMousePosition;
+
+        delegate void LogAppendDelegate(Color color, string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.Manual;
+            int ScreenWidth = SystemInformation.VirtualScreen.Width;
+            int ScreenHeight = SystemInformation.VirtualScreen.Height;
+            int x = ScreenWidth - Width - 20;
+            int y = ScreenHeight / 3 - Height;
+            Location = new Point(x, y);
+            keyboardHook = new KeyboardHook();
+            keyboardHook.KeyDownEvent += new KeyEventHandler(Form1_KeyDown);
+            keyboardHook.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((int)ModifierKeys == (int)Keys.Alt)
+            {
+                switch (e.KeyValue)
+                {
+                    case (int)Keys.S:
+                        startMousePosition = MousePosition;
+                        index = 0;
+                        Log($"已标记洞口坐标：{startMousePosition}", Color.Green);
+                        break;
+                    case (int)Keys.E:
+                        endMousePosition = MousePosition;
+                        index = 0;
+                        Log($"已标记白球坐标：{endMousePosition}", Color.Green);
+                        break;
+                    case (int)Keys.F:
+                        SetAimPos();
+                        break;
+                }
+            }
+        }
+
+        private void SetAimPos()
+        {
+            if (index >= 4)
+            {
+                index = 1;
+            }
+            else
+            {
+                index++;
+            }
+            int x = 0;
+            int y = 0;
+            switch (index)
+            {
+                case 1:
+                    x = (int)(endMousePosition.X + ra * ((endMousePosition.X - startMousePosition.X) / Math.Sqrt((endMousePosition.X - startMousePosition.X) * (endMousePosition.X - startMousePosition.X) + (endMousePosition.Y - startMousePosition.Y) * (endMousePosition.Y - startMousePosition.Y))));
+                    y = (int)(endMousePosition.Y + ra * ((endMousePosition.Y - startMousePosition.Y) / Math.Sqrt((endMousePosition.X - startMousePosition.X) * (endMousePosition.X - startMousePosition.X) + (endMousePosition.Y - startMousePosition.Y) * (endMousePosition.Y - startMousePosition.Y))));
+                    break;
+                case 2:
+                    x = (int)(endMousePosition.X - ra * ((-endMousePosition.X + startMousePosition.X) / Math.Sqrt((-endMousePosition.X + startMousePosition.X) * (-endMousePosition.X + startMousePosition.X) + (endMousePosition.Y - startMousePosition.Y) * (endMousePosition.Y - startMousePosition.Y))));
+                    y = (int)(endMousePosition.Y + ra * ((endMousePosition.Y - startMousePosition.Y) / Math.Sqrt((-endMousePosition.X + startMousePosition.X) * (-endMousePosition.X + startMousePosition.X) + (endMousePosition.Y - startMousePosition.Y) * (endMousePosition.Y - startMousePosition.Y))));
+                    break;
+                case 3:
+                    x = (int)(endMousePosition.X + ra * ((endMousePosition.X - startMousePosition.X) / Math.Sqrt((endMousePosition.X - startMousePosition.X) * (endMousePosition.X - startMousePosition.X) + (-endMousePosition.Y + startMousePosition.Y) * (-endMousePosition.Y + startMousePosition.Y))));
+                    y = (int)(endMousePosition.Y - ra * ((-endMousePosition.Y + startMousePosition.Y) / Math.Sqrt((endMousePosition.X - startMousePosition.X) * (endMousePosition.X - startMousePosition.X) + (-endMousePosition.Y + startMousePosition.Y) * (-endMousePosition.Y + startMousePosition.Y))));
+                    break;
+                case 4:
+                    x = (int)(endMousePosition.X - ra * ((-endMousePosition.X + startMousePosition.X) / Math.Sqrt((-endMousePosition.X + startMousePosition.X) * (-endMousePosition.X + startMousePosition.X) + (-endMousePosition.Y + startMousePosition.Y) * (-endMousePosition.Y + startMousePosition.Y))));
+                    y = (int)(endMousePosition.Y - ra * ((-endMousePosition.Y + startMousePosition.Y) / Math.Sqrt((-endMousePosition.X + startMousePosition.X) * (-endMousePosition.X + startMousePosition.X) + (-endMousePosition.Y + startMousePosition.Y) * (-endMousePosition.Y + startMousePosition.Y))));
+                    break;
+            }
+            SetCursorPos(x, y);
+            Log($"【方案{index}】已设置瞄准坐标：{x}，{y}", Color.Green);
+        }
+
+        private void LogAppend(Color color, string text)
+        {
+            rtbConsole.SelectionColor = color;
+            rtbConsole.AppendText(text);
+            rtbConsole.AppendText("\n");
+            rtbConsole.SelectionStart = rtbConsole.Text.Length;
+            rtbConsole.ScrollToCaret();
+        }
+
+        public void Log(string text, Color color)
+        {
+            LogAppendDelegate la = LogAppend;
+            rtbConsole.Invoke(la, color, DateTime.Now.ToString("HH:mm:ss ") + text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Log("【作弊可耻，公平游戏！！！】", Color.Red);
+            Log("【此项目仅限学习参考！！！】", Color.Red);
+            Log("辅助器已开启", Color.Green);
+        }
+    }
+}
